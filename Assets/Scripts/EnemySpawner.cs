bb1a0e9fa225ce860c0b0c938f148c7648458dcf
alextudell <92903733+alextudell@users.noreply.github.com>
2022-10-01 15:35:08 +0300

    SpawnEnemy return value

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -7,12 +7,20 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField][Range(0.1f, 10)]
     private float _timeToSpawn;
     [SerializeField]
-    private GameObject _enemy;
+    private GameObject _enemyPref;
 
     private int _spawnCount;
 
-    private IEnumerator SpawnEnemy()
+    private IEnumerator SpawnEnemy(int enemyCount)
     {
-        
+        _spawnCount++;
+
+        for (int i = 0; i == enemyCount; i++)
+        {
+            var enemy = Instantiate(_enemyPref);
+            enemy.transform.SetParent(gameObject.transform,false); 
+        }
+
+        yield return null;
     }
 }
