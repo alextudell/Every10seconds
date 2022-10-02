b6bff3ecacd19a3d0b9d70ab921fc2d553b501d8
alextudell <92903733+alextudell@users.noreply.github.com>
2022-10-02 17:30:31 +0300

    Another commit

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -136,7 +136,6 @@ public class EnemyController : MonoBehaviour
         }
         
         Destroy(gameObject);
-        Destroy(bullet.gameObject);
         _timeManager.GetTimeForKilling();
     }
 
